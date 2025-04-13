3a0dea030fefe859648f1c46ee46e0b8b01047e5
AbdelrahmanAbouzaid <a.abouzaid404@gmail.com>
2025-04-13 21:26:43 +0200

    Add Create, Update And Delete Property

--- DigitalPropertyManagementBLL/Interfaces/IPropertyRepository.cs
+++ DigitalPropertyManagementBLL/Interfaces/IPropertyRepository.cs
@@ -12,6 +12,11 @@ namespace DigitalPropertyManagementBLL.Interfaces
     {
         Task<IEnumerable<Property>> GetAllAsync(PropertyFilterDto filter);
         Task<Property> GetDetails(int id);
+        Task AddAsync(Property property);
+        Task<Property?> GetByIdAsync(int id);
+        void Update(Property property);
+        void Delete(Property property);
+       
         Task<IEnumerable<Property>> GetPropertiesByGovernorateAsync(string governorate);
         Task<IEnumerable<Property>> GetPropertiesByCityAsync(string city);
         Task<IEnumerable<Property>> GetPropertiesByTypeAsync(string propertyType);
---
+++ GP_DigitalPropertyManegmentApi/Controllers/PropertyUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿namespace GP_DigitalPropertyManegmentApi.Controllers
+{
+    public class PropertyUpdateDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string PropertyType { get; set; }
+        public double Size { get; set; }
+        public int Bedrooms { get; set; }
+        public int Bathrooms { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Governate { get; set; }
+    }
+}
